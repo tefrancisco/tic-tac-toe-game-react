9e68e856629700a89b1de01ffcb36f12624dda74
tefrancisco <dzfranciscodz@gmail.com>
2025-12-07 18:12:39 -0300

    Fixed bug of clicking the same button multiple times

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -77,7 +77,7 @@ export default function GameBoard({ onSelectSquare, turns }) {
             {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
                 <ol>
                     {row.map((playerSymbol, colIndex) => <li key={colIndex}>
-                        <button onClick={() => onSelectSquare(rowIndex, colIndex)} >{playerSymbol}</button>
+                        <button onClick={() => onSelectSquare(rowIndex, colIndex)} disabled={playerSymbol !== null} >{playerSymbol}</button>
                     </li>)}
                 </ol>
             </li>)}
